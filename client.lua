62765834ea8eba277700486146c9d9ce154c87a7
Josh <88961456+JoshRiang@users.noreply.github.com>
2022-02-18 22:15:36 +0800

    Add files via upload

--- client.lua
+++ client.lua
@@ -11,24 +11,42 @@ end
 RegisterCommand('setspawn', function()
     local ped = PlayerPedId()
     local coord = GetEntityCoords(ped)
-    TriggerEvent("chat:addSuggestion", "/Set Coordinat Point For Return")
+    TriggerEvent('chat:addSuggestion', '/setspawn', 'Setspawn Point', {})
     coords = coord
-    ShowAboveRadarMessage('~g~Spawnpoint set')
+    ShowAboveRadarMessage('~g~~bold~Spawnpoint~s~ set')
 end, false)
 
 RegisterCommand('r', function()
     local ped = PlayerPedId()
-    TriggerEvent("chat:addSuggestion", "/Return To Coordinat Point You've Set")
+    local vehicle = GetVehiclePedIsIn(ped, false)
+    local seat = 
+    TriggerEvent('chat:addSuggestion', '/r', 'Return To Spawn Point', {})
     if coords ~= nil then 
-        SetEntityCoords(ped, coords, false, false, false, false)
-        ShowAboveRadarMessage('Returned To Coordinat Point')
+        if IsPedInAnyVehicle(ped, true) then
+            if GetPedInVehicleSeat(vehicle, -1) then
+                SetEntityCoords(ped, coords, false, false, false, false)
+                SetEntityCoordsNoOffset(vehicle, coords, 0, 0, 1)
+                SetPedIntoVehicle(ped, vehicle, -1)
+                ShowAboveRadarMessage('~g~~bold~Returned~s~ To Coordinat Point')
+            elseif not GetPedInVehicleSeat(vehicle, -1) then
+                ShowAboveRadarMessage('You Not In Driver Seat')
+            end
+        else
+            SetEntityCoords(ped, coords, false, false, false, false)
+            ShowAboveRadarMessage('~g~~bold~Returned~s~ To Coordinat Point')
+        end
     else
-        ShowAboveRadarMessage('No Spawn Point Detected')
+        ShowAboveRadarMessage('~r~~bold~No Spawn Point Detected')
     end
 end, false)
 
 RegisterCommand('deletecoords', function()
-    coords = nil
+    if coords ~= nil then
+        coords = nil
+        ShowAboveRadarMessage('Spawn Point ~r~~bold~Deleted')
+    else 
+        ShowAboveRadarMessage('~r~~bold~No Spawn Point Detected')
+    end
 end, false)
 
 RegisterKeyMapping('setspawn', 'Setspawn', 'keyboard', setspawn)
